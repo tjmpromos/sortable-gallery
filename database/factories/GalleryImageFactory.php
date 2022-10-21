1b9b451c134d58da70e44ae3a7f1a98c7f2e93fe
Mike Wall <daikazu@gmail.com>
2022-10-21 14:31:38 -0400

    factory

---
+++ database/factories/GalleryImageFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tjmpromos\SortableGallery\Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Tjmpromos\SortableGallery\Models\GalleryImage;
+
+
+class GalleryImageFactory extends Factory
+{
+
+    protected $model = GalleryImage::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+
+    public function definition(): array
+    {
+        return [
+            'name'      => fake()->name(),
+            'is_active' => fake()->boolean(),
+        ];
+    }
+}

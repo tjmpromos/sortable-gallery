e85b170f0baa8e94c6ff9b3891429ed07daa0bb2
Cory Dymond <hi@corydymond.com>
2023-02-16 08:16:40 -0500

    adding pagination amount to this config instead of website

--- config/sortable-gallery.php
+++ config/sortable-gallery.php
@@ -8,6 +8,11 @@ return [
         'disk_name' => 'public',
     ],
 
+	/**
+	 *   Set amount of images per page in the gallery
+	 */
+	'images_per_page' => 16,
+
     /**
      * Tag types used to group gallery image filters. These are used to create the
      * tag input fields on the gallery image resource forms. This package uses
